3402568ae1fcf60bde8f695f1140ec50e1139628
José <joseraimundoalves@hotmail.com>
2025-11-11 19:54:52 -0300

    funcionando

--- dma.c
+++ dma.c
@@ -1,9 +1,13 @@
 //////////////////////////////////////////////////////////////////////////////
 // * File name: dma.c
-// * // * Description:  DMA functions. (MODIFICADO PARA Rx-Tx ÁUDIO LOOPBACK)
-// * // * Copyright (C) 2011 Texas Instruments Incorporated - http://www.ti.com/ 
+// *
+// * Description:  Funções DMA. (MODIFICADO PARA Rx-Tx ÁUDIO LOOPBACK)
+// * (ESTRUTURA PING-PONG IMPLEMENTADA)
+// *
+// * Copyright (C) 2011 Texas Instruments Incorporated - http://www.ti.com/
 // * Copyright (C) 2011 Spectrum Digital, Incorporated
-// * //////////////////////////////////////////////////////////////////////////////
+// *
+// //////////////////////////////////////////////////////////////////////////////
 #include"ezdsp5502.h"
 #include"ezdsp5502_mcbsp.h"
 #include "csl_dma.h"
@@ -11,21 +15,28 @@
 
 Uint8 dmaState = 0; // (Não mais usado, mas mantido por segurança)
 
-
-#define AUDIO_BUFFER_SIZE 4096 // Manter o mesmo tamanho do demo
+// =================== ALTERAÇÃO PING-PONG ===================
+// O tamanho total do buffer é 4096, mas vamos processá-lo
+// em blocos de 2048 (metade).
+#define AUDIO_BUFFER_SIZE 4096 // Tamanho TOTAL (deve ser par)
+#define AUDIO_BLOCK_SIZE (AUDIO_BUFFER_SIZE / 2) // Tamanho do bloco (Ping ou Pong)
+// =========================================================
 
 #pragma DATA_SECTION(g_rxBuffer, "dmaMem")
 #pragma DATA_ALIGN(g_rxBuffer, 4096)
-Uint16 g_rxBuffer[AUDIO_BUFFER_SIZE]; // Onde o "Line In" escreve (BUFFER DE ENTRADA)
+Uint16 g_rxBuffer[AUDIO_BUFFER_SIZE]; // Buffer circular de ENTRADA
 
 #pragma DATA_SECTION(g_txBuffer, "dmaMem")
 #pragma DATA_ALIGN(g_txBuffer, 4096)
-Uint16 g_txBuffer[AUDIO_BUFFER_SIZE]; // De onde o "Headphone" lê (BUFFER DE SAÍDA)
+Uint16 g_txBuffer[AUDIO_BUFFER_SIZE]; // Buffer circular de SAÍDA
 // =========================================================================
 
 
 /* DMA configuration structure ( Tx - Transmissão) */
-DMA_Config dmaTxConfig = { // (Renomeado de 'myconfig' para 'dmaTxConfig')
+// O Tx não precisa de interrupções. Ele simplesmente corre em loop
+// contínuo, lendo o g_txBuffer de 0 a 4095 e repetindo.
+// A CPU irá escrever neste buffer "ao mesmo tempo".
+DMA_Config dmaTxConfig = {
     DMA_DMACSDP_RMK(
         DMA_DMACSDP_DSTBEN_NOBURST , // Destination burst
         DMA_DMACSDP_DSTPACK_OFF,     // Destination packing
@@ -53,8 +64,8 @@ DMA_Config dmaTxConfig = { // (Renomeado de 'myconfig' para 'dmaTxConfig')
         DMA_DMACICR_AERRIE_OFF,
         DMA_DMACICR_BLOCKIE_OFF ,    // Whole block interrupt enable
         DMA_DMACICR_LASTIE_OFF,      // Last frame Interrupt enable
-        DMA_DMACICR_FRAMEIE_OFF,     // (Lição: Interrupção de Tx DESLIGADA)
-        DMA_DMACICR_FIRSTHALFIE_OFF, // HAlf frame interrupt enable
+        DMA_DMACICR_FRAMEIE_OFF,     // (Tx NÃO precisa de interrupção)
+        DMA_DMACICR_FIRSTHALFIE_OFF, // (Tx NÃO precisa de interrupção)
         DMA_DMACICR_DROPIE_OFF,      // Sync event drop interrupt enable
         DMA_DMACICR_TIMEOUTIE_OFF    // Time out inetrrupt enable
     ), /* DMACICR */
@@ -63,12 +74,12 @@ DMA_Config dmaTxConfig = { // (Renomeado de 'myconfig' para 'dmaTxConfig')
     0,                  // DMACSSAU
     (DMA_AdrPtr)0x5804, // DMACDSAL - DMA destination is DXR1 (Transmit)
     0,                  // DMACDSAU 
-    AUDIO_BUFFER_SIZE,  // DMACEN  - 1024 elementos
+    AUDIO_BUFFER_SIZE,  // DMACEN  - Transferência de 4096 elementos
     1,                  // DMACFN  - Single frame
     0,                  // DMACSFI - Source frame index
     0,                  // DMACSEI - Source element index
     0,                  // DMACDFI - Destination frame index
-    0                   // DMACDEI - (Demo usava 2, mas 0 é mais seguro)
+    0                   // DMACDEI
 };
 
 
@@ -101,21 +112,24 @@ DMA_Config dmaRxConfig = {
         DMA_DMACCR_SYNC_REVT1        /* Sincronizar com o Evento de RECEÇÃO (Rx) */
     ), /* DMACCR */
 
+    // =================== ALTERAÇÃO PING-PONG ===================
+    // Ativamos AMBAS as interrupções: Meio-Buffer e Fim-Buffer
     DMA_DMACICR_RMK(
         DMA_DMACICR_AERRIE_OFF,
         DMA_DMACICR_BLOCKIE_OFF,
         DMA_DMACICR_LASTIE_OFF,
-        DMA_DMACICR_FRAMEIE_ON,
-        DMA_DMACICR_FIRSTHALFIE_OFF,
+        DMA_DMACICR_FRAMEIE_ON,      /* Interrupção de FIM (processar Bloco PONG) */
+        DMA_DMACICR_FIRSTHALFIE_ON,  /* Interrupção de MEIO (processar Bloco PING) */
         DMA_DMACICR_DROPIE_OFF,
         DMA_DMACICR_TIMEOUTIE_OFF
     ), /* DMACICR */
+    // =========================================================
 
     (DMA_AdrPtr)0x5800, // DMACSSAL - Endereço do McBSP DRR1 (Receive)
     0,                  // DMACSSAU
     (DMA_AdrPtr)0x0000, // DMACDSAL - (Vamos definir isto na configAudioDma)
     0,                  // DMACDSAU
-    AUDIO_BUFFER_SIZE,  // DMACEN  - 1024 elementos
+    AUDIO_BUFFER_SIZE,  // DMACEN  - Tamanho total do buffer circular (4096)
     1,                  // DMACFN  - Single frame
     0,                  // DMACSFI
     0,                  // DMACSEI
@@ -126,7 +140,14 @@ DMA_Config dmaRxConfig = {
 //(ISRs):
 extern void VECSTART(void);
 interrupt void dmaRxIsr(void);
-interrupt void dmaTxIsr(void);
+interrupt void dmaTxIsr(void); // (Não usada, mas precisa de existir)
+
+// =================== ALTERAÇÃO PING-PONG ===================
+// Flag estática para sabermos qual bloco processar.
+// 0 = Processar Bloco PING (primeira metade)
+// 1 = Processar Bloco PONG (segunda metade)
+static volatile Uint16 dmaPingPongFlag = 0;
+// =========================================================
 
 /*
  * configAudioDma( )
@@ -137,9 +158,8 @@ interrupt void dmaTxIsr(void);
 void configAudioDma (void)
 {
     Uint16 rcvEventId, xmtEventId;
-
     
-    // Aponta o Tx para o nosso NOVO buffer de saída (g_txBuffer)
+    // Aponta o Tx para o nosso buffer de saída (g_txBuffer)
     dmaTxConfig.dmacssal = (DMA_AdrPtr)(((Uint32)&g_txBuffer) << 1);
     
     // Canal 0 para Tx
@@ -148,8 +168,7 @@ void configAudioDma (void)
 
     /* --- Configurar o Canal de Receção (Rx - Canal 1) --- */
 
-    //  Apontar o Rx para o nosso NOVO buffer de entrada (g_rxBuffer)
-    // E traduzir o endereço para Byte (<< 1)
+    // Apontar o Rx para o nosso buffer de entrada (g_rxBuffer)
     dmaRxConfig.dmacdsal = (DMA_AdrPtr)(((Uint32)&g_rxBuffer) << 1);
 
     //Canal 1 para Rx
@@ -164,26 +183,27 @@ void configAudioDma (void)
     xmtEventId = DMA_getEventId(dmaTxHandle);
     
     IRQ_disable(rcvEventId);
-    IRQ_disable(xmtEventId);
+    IRQ_disable(xmtEventId); // (Tx não terá interrupções)
     IRQ_clear(rcvEventId);
     IRQ_clear(xmtEventId);
 
-    IRQ_plug(rcvEventId, &dmaRxIsr); // Ligar
-    IRQ_plug(xmtEventId, &dmaTxIsr); // Ligar
+    IRQ_plug(rcvEventId, &dmaRxIsr); // Ligar ISR de Receção
+    IRQ_plug(xmtEventId, &dmaTxIsr); // Ligar ISR de Transmissão (stub)
 
     IRQ_enable(rcvEventId); // Ligar APENAS a interrupção de Receção
-
     
     IRQ_globalEnable();
 }
 
 void startAudioDma (void)
 {
+    // Resetar o flag PingPong antes de começar
+    dmaPingPongFlag = 0;
+
     DMA_start(dmaRxHandle); // Ligar RECEPÇAO
     DMA_start(dmaTxHandle); // Ligar TRANSMISSAO
 }
 // =========================================================================
-
 
 
 void stopAudioDma (void)
@@ -199,31 +219,49 @@ void changeTone (void)
     // (Isto impede o projeto de "explodir" se o SW2 for premido)
 }
 
-/*
- * dmaRxIsr( )
- *
- * O DMA chama esta função QUANDO o g_rxBuffer está CHEIO.
- * Este é o "Cérebro" do nosso projeto.
+
  */
 interrupt void dmaRxIsr(void)
 {
     int i;
+    Uint16 *p_rxBuffer; // Ponteiro para o início do bloco de ENTRADA
+    Uint16 *p_txBuffer; // Ponteiro para o início do bloco de SAÍDA
 
-    /* O "Cérebro" (Processamento) */
-    // Por agora, vamos apenas fazer um loopback (bypass)
-    // O áudio que acabou de chegar (g_rxBuffer)
-    // é copiado para o áudio que vai sair (g_txBuffer)
-
-    for (i = 0; i < AUDIO_BUFFER_SIZE; i++)
+    if (dmaPingPongFlag == 0)
     {
-        // (No futuro, o seu Reverb/Phaser irá aqui)
-        g_txBuffer[i] = g_rxBuffer[i];
+        // --- Processar Bloco PING ---
+        // O DMA acabou de encher a PRIMEIRA METADE (Ping) [0 ... 2047]
+        // O DMA está AGORA a encher a SEGUNDA METADE (Pong)
+
+        p_rxBuffer = &g_rxBuffer[0];                // Ler de g_rxBuffer[0]
+        p_txBuffer = &g_txBuffer[0];                // Escrever em g_txBuffer[0]
+
+        dmaPingPongFlag = 1; // Na próxima interrupção, processar o PONG
+    }
+    else
+    {
+        // --- Processar Bloco PONG ---
+        // O DMA acabou de encher a SEGUNDA METADE (Pong) [2048 ... 4095]
+        // O DMA está AGORA a encher a PRIMEIRA METADE (Ping)
+
+        p_rxBuffer = &g_rxBuffer[AUDIO_BLOCK_SIZE]; // Ler de g_rxBuffer[2048]
+        p_txBuffer = &g_txBuffer[AUDIO_BLOCK_SIZE]; // Escrever em g_txBuffer[2048]
+
+        dmaPingPongFlag = 0; // Na próxima interrupção, processar o PING
     }
 
 
+    for (i = 0; i < AUDIO_BLOCK_SIZE; i++)
+    {
+        // (No futuro, o seu Reverb/Phaser irá aqui)
+        p_txBuffer[i] = p_rxBuffer[i];
+    }
 }
 
-/* Transmissão. Não a usamos, mas tem de existir. */
+/* * dmaTxIsr( )
+ * Transmissão. Não a usamos, mas tem de existir porque
+ * foi ligada (plugged) ao vetor de interrupção.
+ */
 interrupt void dmaTxIsr(void)
 {
     // Não faz nada
