53c1b21cf21c07daf8926db677b723e7232645c5
José <joseraimundoalves@hotmail.com>
2025-11-17 11:28:13 -0300

    igualando parametes branchs

--- tremolo_params.h
+++ tremolo_params.h
@@ -1,13 +1,26 @@
+/*
+ * ============================================================================
+ * flanger_params.h
+ *
+ * Contém todas as macros de configuração (#define) para 
+ * os parâmetros dos efeitos de áudio.
+ * ============================================================================
+ */
 
 #ifndef __TREMOLO_PARAMS_H__
 #define __TREMOLO_PARAMS_H__
 
 
-#define TREMOLO_fr    6 //Taxa de Modulação FR
-#define TREMOLO_DEPTH_Q15  16384 // Amplitude -> valores estão em Q15 (16385 = 0.5)
+//--------------- PARÂMETROS DO EFEITO TREMOLO ------------------
+// (Velocidade do LFO (6 Hz é um bom valor para tremolo))
+#define TREMOLO_fr    6
 
-#define TREMOLO_OFFSET_Q15 32767 //1 em q15
-#define TREMOLO_INPUT_GAIN_Q15 32767 //1 em q15
+// (Profundidade: 0.5 em Q15, conforme realtime_tremolo.c)
+#define TREMOLO_DEPTH_Q15  16384 
+// (Offset: 1.0 - 0.5 = 0.5 em Q15)
+#define TREMOLO_OFFSET_Q15 16384 
+// (Ganho de entrada: 0.7 em Q15, conforme realtime_tremolo.c)
+#define TREMOLO_INPUT_GAIN_Q15 32767 // (0.7 * 32767) 22936
 
 
 #endif // __TREMOLO_PARAMS_H__
