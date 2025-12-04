fff58a5a28af5ecccc1c77b42c1f1bfcf9dab6d0
Joserasj <joseraimundoalves@hotmail.com>
2025-12-04 10:05:25 -0300

    comentarios reverb params

--- reverb_params.h
+++ reverb_params.h
@@ -11,6 +11,8 @@
 // --- TAMANHOS DOS ATRASOS (Delay Lengths) ---
 // Escolhidos para serem números primos (Prime Numbers) para evitar metalização.
 // Baseado em ~48kHz.
+// Para achar em ms divide por 48
+// Varia de 0 a 8160
 #define C1_LEN 1687 // ~35ms
 #define C2_LEN 1601 // ~33ms
 #define C3_LEN 2053 // ~42ms
@@ -21,7 +23,9 @@
 
 // --- GANHOS (Q15) ---
 // Feedback dos Combs (Controla o tempo de reverberação RT60)
-// 0.8 a 0.85 é um bom valor. (0.84 * 32767 = 27524)
+// 0.8 a 0.85 é um bom valor. (0.84 * 32767 = 27524) Q15
+// Varia de 0 a 32767 (maximo)
+
 #define C1_GAIN 27524
 #define C2_GAIN 27524
 #define C3_GAIN 27524
