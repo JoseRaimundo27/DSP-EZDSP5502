879b976b357403da2ceb79654e9ca57fa91539b7
José <joseraimundoalves@hotmail.com>
2025-12-13 14:36:09 -0300

    alterando parametros do phaser

--- CCS/phaser_params.h
+++ CCS/phaser_params.h
@@ -5,12 +5,12 @@
 // (O "LFO" de Ponto Fixo (Tabela de Seno))
 #define LFO_SIZE 256
 // Número de filtros em cascata (8 = Som denso e profundo)
-#define PHASER_NUM_STAGES 8
+#define PHASER_NUM_STAGES 1
 
 // Feedback (Ressonância).
 // Valor Q15: 0.85 * 32768 = 27853
 // Quanto maior, mais "metálico" e "assobiante" o som fica.
-#define PHASER_FEEDBACK_Q15 27853
+#define PHASER_FEEDBACK_Q15 10000
 
 // Faixa de Varredura (Sweep Range) do efeito
 // Define o quão grave e agudo o efeito chega.
@@ -19,6 +19,6 @@
 #define PHASER_MIN_ALPHA_Q15 3277
 
 // Max Alpha (0.9) -> 0.9 * 32768 = 29491
-#define PHASER_MAX_ALPHA_Q15 29491
+#define PHASER_MAX_ALPHA_Q15 3277
 
 #endif /* PHASER_PARAMS_H_ */
